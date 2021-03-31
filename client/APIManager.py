6a99c7b65b775bb786842b9f93f8e6d25f49782d
Pascal Schlatter <copifybot@gmail.com>
2021-03-31 21:14:09 +0200

    Moved example to root directory

--- client/APIManager.py
+++ client/APIManager.py
@@ -5,6 +5,7 @@ class APIManager:
     def __init__(self, apiEndpoint):
         self.requester = APIRequest.APIRequest(apiEndpoint)
 
+    # This is the abstracted functionality that is exposed to users
     def verifyKey(self, key):
         return self.requester.verifyKey(key)
     
